cd7cf8dd4feadb23e5289b2ae9a252beaa7456bb
Kevin Doyle <kevindoyleie@gmail.com>
2021-02-01 17:18:46 +0000

    Adding mediator example

---
+++ src/designpatterns/mediator/Storage.java
@@ -0,0 +1,17 @@
+package designpatterns.mediator;
+
+public class Storage<T>
+{
+    T value;
+
+    T getValue()
+    {
+        return value;
+    }
+
+    void setValue(Mediator<T> mediator, String storageName, T value)
+    {
+        this.value = value;
+        mediator.notifyObservers(storageName);
+    }
+}
